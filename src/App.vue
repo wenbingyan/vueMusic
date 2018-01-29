5ca178af6c056567cd034d8945d1226cb2c13443
unknown <wen_bingyan@DESKTOP-KIK0J1R.xiseg.com>
2018-01-29 18:34:57 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import MHeader from './components/m-header'
+  import Tab from './components/tab'
+
+  export default {
+    components: {
+      MHeader,
+      Tab
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
